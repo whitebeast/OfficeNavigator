c367214dcad4351495783765a8c0bcd62c5ae49a
Igor Tatarnikov <dexter.godspeed@gmail.com>
2016-10-18 23:27:56 +0300

    Default Solution structure

---
+++ sources/OfficeNav/OfficeNav.Tests.WebApi/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeNav.WebApi;
+using OfficeNav.WebApi.Controllers;
+
+namespace OfficeNav.Tests.WebApi.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ sources/OfficeNav/OfficeNav.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace OfficeNav.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
